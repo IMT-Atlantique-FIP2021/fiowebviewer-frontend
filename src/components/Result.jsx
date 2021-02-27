cd53b95b13be3652e5f20f66b6a6baae309c680f
GuillaumePEILLER <guillaume.peiller@imt-atlantique.net>
2021-02-27 18:07:26 +0000

    graph is good

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,5 +1,6 @@
-import { Component } from "react";
+import { Component, PureComponent } from "react";
 import { ChevronRight, ChevronDown } from "react-feather";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 export default function TestingComponent() {
     return (
@@ -17,8 +18,8 @@ export default function TestingComponent() {
                     </div>
                     <div>
                         <Table tableHeader={<TableJobsName />}>
-                            <div>
-                                BIG GRAPH
+                            <div className="h-80 p-5">
+                                {TableJobsValue()}
                             </div>
                         </Table>
                     </div>
@@ -33,7 +34,7 @@ class Table extends Component {
     constructor(props) {
         super(props);
         this.onVisibilityChange = this.onVisibilityChange.bind(this)
-        this.state = { isReduced: true }
+        this.state = { isReduced: false }
     }
 
     onVisibilityChange() {
@@ -115,7 +116,7 @@ class TableHeader extends Component {
     }
 }
 
-
+//CONTENT FOR TEST NAME TABLE
 function TableTestName(props) {
     return (
         <div>{props.title || "FIO Test Name"}</div>
@@ -167,9 +168,91 @@ function TableTestCsvValue() {
     );
 }
 
-
+//CONTENT FOR 
 function TableJobsName(props) {
     return (
         <div>{props.title || "Jobs"}</div>
     );
+}
+function TableJobsValue() {
+    return(
+
+        <Graph/>
+    );
+}
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+ class Graph extends PureComponent {
+  static demoUrl = 'https://codesandbox.io/s/simple-line-chart-kec3v';
+
+  render() {
+    return (
+        
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="linear" dataKey="pv" stroke="#228844" activeDot={{ r: 8 }} />
+          <Line type="linear" dataKey="uv" stroke="#dd2244" />
+        </LineChart>
+      </ResponsiveContainer>
+    );
+  }
 }
