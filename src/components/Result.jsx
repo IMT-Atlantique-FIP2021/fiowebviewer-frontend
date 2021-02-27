54197d849ba7bebfa10790047a2a0bea834dc9fc
GuillaumePEILLER <guillaume.peiller@imt-atlantique.net>
2021-02-27 20:36:01 +0000

    Jobs graph basic

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -18,8 +18,13 @@ export default function TestingComponent() {
                     </div>
                     <div>
                         <Table tableHeader={<TableJobsName />}>
-                            <div className="h-80 p-5">
+                        <div className="grid grid-cols-4 auto-layout">
+                            <div className="col-span-1 border-r-2 ">
+                                NEED CHECKBOX
+                            </div>
+                            <div className="col-span-3">
                                 {TableJobsValue()}
+                                </div>
                             </div>
                         </Table>
                     </div>
@@ -175,84 +180,92 @@ function TableJobsName(props) {
     );
 }
 function TableJobsValue() {
-    return(
+    return (
 
-        <Graph/>
+        <div className="h-80 p-5" >
+            <div className="text-center">
+            Hello
+            </div>
+        <Graph />
+        </div>
     );
 }
 
+
+
 const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
 ];
 
- class Graph extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/simple-line-chart-kec3v';
 
-  render() {
-    return (
-        
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="linear" dataKey="pv" stroke="#228844" activeDot={{ r: 8 }} />
-          <Line type="linear" dataKey="uv" stroke="#dd2244" />
-        </LineChart>
-      </ResponsiveContainer>
-    );
-  }
+
+class Graph extends PureComponent {
+    static demoUrl = 'https://codesandbox.io/s/simple-line-chart-kec3v';
+
+    render() {
+        return (
+            <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend  />
+                    <Line type="linear" dataKey="pv" stroke="#228844" activeDot={{ r: 8 }} />
+                    <Line type="linear" dataKey="uv" stroke="#dd2244" />
+                </LineChart>
+            </ResponsiveContainer>
+        );
+    }
 }
