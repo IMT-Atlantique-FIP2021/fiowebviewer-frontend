9fc680b731781bf4258052c0054d4ebb7c1a8053
sebastienHUGDELARAUZE <sebastien.hugdelarauze@gmail.com>
2021-03-10 10:07:49 +0100

    Fix delete button activation > When one result is selected

--- src/components/ResultTable.tsx
+++ src/components/ResultTable.tsx
@@ -308,7 +308,7 @@ function TableDeleteButton(selectedResults: string[], onClick: () => void) {
                 }}
             >
                 <button
-                    {...{ disabled: selectedResults.length <= 1 }}
+                    {...{ disabled: selectedResults.length <= 0 }}
                     onClick={onClick}
                     className="disabled:opacity-50 disabled:bg-gray-400 bg-red-500 hover:opacity-100 opacity-80 p-2 w-64 font-semibold border rounded text-white"
                 >
